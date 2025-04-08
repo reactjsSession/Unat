7c63ebce8e4cd63008a6489203cf555d969f1a60
unat-pannu <unatkaur.pannu@bcah.christuniversity.in>
2025-04-08 17:17:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import ParentComponent from './ParentComponent'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <ParentComponent/>
     </>
   )
 }
-
 export default App
